0043ee41104d2b1a2b26f0f2524bbb27bcef53bf
Admin <prachimshewale26@gmail.com>
2025-05-07 18:57:28 +0530

    commit 1

---
+++ src/main/java/com/habitHatch/HabitHatchApplication.java
@@ -0,0 +1,13 @@
+package com.habitHatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitHatchApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitHatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitHatch/HabitHatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitHatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabbitHatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
