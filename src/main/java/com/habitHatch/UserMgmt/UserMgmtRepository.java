395cce71bb1d539048f99e67185727c44a43c362
Suman Bej <sumanbej.glt@gmail.com>
2025-05-07 19:10:59 +0530

    second commit

--- src/main/java/com/habitHatch/UserMgmt/UserMgmtRepository.java
+++ src/main/java/com/habitHatch/UserMgmt/UserMgmtRepository.java
@@ -2,5 +2,6 @@ package com.habitHatch.UserMgmt;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserMgmtRepository {
+public interface UserMgmtRepository extends CRUDRepository<User, String> {
+    // Define any additional query methods if needed
 }
