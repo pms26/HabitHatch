ff2d9ddae24e5799f2c45a906418e50500df265e
Admin <prachimshewale26@gmail.com>
2025-05-07 18:47:00 +0530

    commit 1

---
+++ src/main/java/com/habitHatch/UserMgmt/UserMgmtController.java
@@ -0,0 +1,10 @@
+package com.habitHatch.UserMgmt;
+
+public class UserMgmtController {
+    //post
+        //name ,ph no., email, password
+    //patch
+    //get
+    //get details
+    //delete
+}
---
+++ src/main/java/com/habitHatch/UserMgmt/UserMgmtService.java
@@ -0,0 +1,4 @@
+package com.habitHatch.UserMgmt;
+
+public class UserMgmtService {
+}
