d84d0fbdc1ec75972e2a1f2dc7e25c7cb9309c62
Admin <prachimshewale26@gmail.com>
2025-05-22 22:37:47 +0530

    swagger success on local host url

--- src/main/java/com/habitHatch/UserMgmt/entity/UserDetailsRequest.java
+++ src/main/java/com/habitHatch/UserMgmt/entity/UserDetailsRequest.java
@@ -1,13 +1,16 @@
 package com.habitHatch.UserMgmt.entity;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@Schema(description = "User Details Request")
 public class UserDetailsRequest {
+
     private Integer weight;
     private Integer height;
     private Integer age;
