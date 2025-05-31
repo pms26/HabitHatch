3f124c4b2bfc84c672f5fb8c5e1f23d6820ebd0a
Admin <prachimshewale26@gmail.com>
2025-05-31 20:13:13 +0530

    kafka properties and local setup

---
+++ src/main/java/com/habitHatch/Kafka/KafkaConfig.java
@@ -0,0 +1,14 @@
+package com.habitHatch.Kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic createTopic() {
+        return TopicBuilder.name("HabitHatchTopic").build();
+    }
+}
