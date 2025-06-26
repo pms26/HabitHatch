51a0576a2c8852672c870c2f1f7823bfb7347a2e
Suman Bej <sumanbej.glt@gmail.com>
2025-06-26 23:01:26 +0530

    user details impl--2

--- src/main/java/com/habitHatch/security/SecurityConfig.java
+++ src/main/java/com/habitHatch/security/SecurityConfig.java
@@ -1,52 +1,33 @@
 package com.habitHatch.security;
 
-import com.habitHatch.UserMgmt.UserMgmtService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
+
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfiguration {
     @Autowired
     @Lazy
     private  UserDetailsService userDetailsService;
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(register -> {
-                    register.requestMatchers("/login").permitAll() // Allow login endpoint
-                            .requestMatchers("/v1/user/**").hasRole("USER")
-                            .anyRequest().authenticated();
-                })
-                .csrf(csrf -> csrf.disable()) // Disable CSRF
-                .formLogin(form -> form.disable()) // Disable form login
-                .build();
-    }
 
-//
-//    @Bean
-//    public UserDetailsService userLogin() {
-//        UserDetails adminUser= User.builder()
-//                .username("user1")
-//                .password("$2a$12$8b8BrCCUKamDjPGQPRQroOOXh8WhE8wpDZcvHJjakCUTtu2Q2u4Sm")
-//                .password(encode().encode("pass@123"))
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(adminUser);
-//    }
     @Bean
     public UserDetailsService userDetailsService(){
-        return userDetailsService;
+        return new CustomUserDetailsService();
     }
     @Bean
     public AuthenticationProvider authenticationProvider(){
@@ -59,5 +40,18 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder encode() {
         return new BCryptPasswordEncoder();}
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+       httpSecurity
+               .csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests(auth -> auth
+                       .requestMatchers("/v1/user/**").hasRole("USER")
+                       .requestMatchers("/login").permitAll()
+                       .anyRequest().authenticated());
+    return httpSecurity.build();
+    }
+
+
 
 }
